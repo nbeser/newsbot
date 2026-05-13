4c4f620cd66b0072f6eb08685bc0576c5eb79a09
Nurettin Beser <nurettinbeser@yahoo.com>
2026-05-13 16:40:07 +0300

    fixed: saving news through dbmanager.py is fixed.

--- check_code.py
+++ check_code.py
@@ -5,12 +5,11 @@ def db_check():
         conn = get_connection()
         cursor = conn.cursor()
         cursor.execute("SELECT * FROM news;")
-        files = [data for data in cursor.fetchall()]
+        files = [data[0] for data in cursor.fetchall()]
         for i in files:
             print(i)
         cursor.close()
         conn.close()
     except Exception as err:
         return err
-
 db_check()
